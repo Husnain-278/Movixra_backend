6a5ebcf3fc4c815ea53a7292d2671aa59d832d14
Husnain-278 <husnain327878@gmail.com>
2026-05-22 00:09:17 -0700

    made changes in url structure

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,6 @@ from .views import MovieListView, MovieDetailView, MovieCastView
 
 urlpatterns = [
     path('', MovieListView.as_view(), name='movie-list'),
-    path('movies/<slug:slug>/', MovieDetailView.as_view(), name='movie-detail'),
-    path('movies/<slug:slug>/cast/', MovieCastView.as_view(), name='movie-cast'),
+    path('<slug:slug>/', MovieDetailView.as_view(), name='movie-detail'),
+    path('<slug:slug>/cast/', MovieCastView.as_view(), name='movie-cast'),
 ]
